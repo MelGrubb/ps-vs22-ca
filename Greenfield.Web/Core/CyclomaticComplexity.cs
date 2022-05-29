2ed91c700e305751a199c93d8d0fdbe208578daa
Mel Grubb <MelGrubb@live.com>
2022-05-29 09:08:57 -0700

    Added CyclomaticComplexity class. Dialed back warnings

---
+++ Greenfield.Web/Core/CyclomaticComplexity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Greenfield.Web.Core
+{
+    public class CyclomaticComplexity
+    {
+        public int One(int left, int right)
+        {
+            int value = left + right;
+
+            return value;
+        }
+
+        public int Two(int left, int right)
+        {
+            int value;
+
+            if (left > right)
+            {
+                value = left + right;
+            }
+            else
+            {
+                value = right - left;
+            }
+
+            return value;
+        }
+
+        public int Three(int left, int right)
+        {
+            int value;
+
+            if (left < right)
+            {
+                if (left < 0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = left + right;
+                }
+            }
+            else
+            {
+                value = right - left;
+            }
+
+            return value;
+        }
+
+        public int Four(int left, int right)
+        {
+            int value;
+
+            if (left > right)
+            {
+                value = left + right;
+            }
+            else
+            {
+                value = right - left;
+            }
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                value = -value;
+            }
+
+            return value;
+        }
+    }
+}
